658276d9f6850809bbbd9e0539c43c51dbb85520
kanki0408 <g2272082@tcu.ac.jp>
2024-09-25 10:07:34 +0900

    追記

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+#flaskモジュールのFlaskクラスをインポートする。
 from flask import Flask
 
+#appにflaskアプリの核を代入している
 app = Flask(__name__)
 
+#指定したURLにアクセスした際に、次の段落の物を実行する
 @app.route('/')
 def hello():
     return "Hello, Flask!"
 
+#app.pyを実行する時、__name__は__main__が代入される
 if __name__ == '__main__':
     app.run()
